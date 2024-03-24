3b9234019e89c18eff611d7fc9be9fe4211549ff
Wo-ogie <siwall0105@gmail.com>
2024-03-25 00:46:07 +0900

    chore: mockito kotlin 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("org.mockito.kotlin:mockito-kotlin:5.2.1")
 }
 
 tasks.withType<KotlinCompile> {
