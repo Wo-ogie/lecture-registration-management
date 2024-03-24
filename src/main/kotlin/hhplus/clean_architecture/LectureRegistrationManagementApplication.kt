c75619db671b968db3345b59e3b98eb6d84feee9
Wo-ogie <siwall0105@gmail.com>
2024-03-24 17:53:58 +0900

    프로젝트 생성 및 초기 설정

---
+++ src/main/kotlin/hhplus/clean_architecture/LectureRegistrationManagementApplication.kt
@@ -0,0 +1,11 @@
+package hhplus.clean_architecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureRegistrationManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<LectureRegistrationManagementApplication>(*args)
+}
